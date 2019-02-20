a089a482ac1d17ccee98e61d2f46af2f03803e70
Alex Swan <alexrswan@gmail.com>
2019-02-20 16:59:38 -0500

    fixed indentation

--- game_functions.py
+++ game_functions.py
@@ -4,6 +4,6 @@ import pygame
 
 def check_events():
     """Respond to keypresses and mouse events"""
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
