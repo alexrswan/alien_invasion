9a243792d43b53d416f00f33b10e8e3191177370
Alex Swan <alexrswan@gmail.com>
2019-03-04 11:20:27 -0500

    added functionality to increase ship speed

--- settings.py
+++ settings.py
@@ -3,7 +3,10 @@ class Settings():
 
     def __init__(self):
         """Initialize the game's settings"""
-        # Screen Settings
+        # Screen settings
         self.screen_width = 900
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
+
+        # Ship settings
+        self.ship_speed_factor = 1.5
